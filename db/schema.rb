1e17465f48ef7ceef5dbad906ce2058f9a79fc14
ogudah <paul.oguda@technekes.com>
2018-04-17 11:58:09 +0300

    Add Account Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180417084948) do
+ActiveRecord::Schema.define(version: 20180417085732) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string "number"
+    t.decimal "balance"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_accounts_on_user_id"
+  end
 
   create_table "reports", force: :cascade do |t|
     t.string "title"
