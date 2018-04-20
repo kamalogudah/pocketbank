0b3694ed1a6784b30a41f48724144f1e750f0da7
ogudah <paul.oguda@technekes.com>
2018-04-20 19:01:39 +0300

    :grimacing:

--- db/migrate/20180418124345_remove_columns_from_transactions.rb
+++ db/migrate/20180418124345_remove_columns_from_transactions.rb
@@ -2,6 +2,6 @@ class RemoveColumnsFromTransactions < ActiveRecord::Migration[5.1]
   def change
     remove_column :transactions, :from, :string
     remove_column :transactions, :to, :string
-    remove_column :transactions, :report, :belongs_to
+    
   end
 end
