0f6bb347e83d2ddf217bd918702834248971af18
ogudah <paul.oguda@technekes.com>
2018-04-20 18:46:31 +0300

    Fix failing heroku issues

--- db/migrate/20180417084948_create_transactions.rb
+++ db/migrate/20180417084948_create_transactions.rb
@@ -6,9 +6,7 @@ class CreateTransactions < ActiveRecord::Migration[5.1]
       t.string :from
       t.string :to
       t.belongs_to :user, foreign_key: true
-      t.belongs_to :report, foreign_key: true
       t.belongs_to :account, foreign_key: true
-
       t.timestamps
     end
   end
