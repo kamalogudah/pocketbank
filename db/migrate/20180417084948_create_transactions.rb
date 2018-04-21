5fbfef64be35eb8b110afbe0db100e6b1fdec97a
ogudah <paul.oguda@technekes.com>
2018-04-22 00:07:01 +0300

    More Housekeeping

--- db/migrate/20180417084948_create_transactions.rb
+++ db/migrate/20180417084948_create_transactions.rb
@@ -7,6 +7,7 @@ class CreateTransactions < ActiveRecord::Migration[5.1]
       t.string :to
       t.belongs_to :user, foreign_key: true
       t.belongs_to :account, foreign_key: true
+      t.belongs_to :category, foreign_key: true
       t.timestamps
     end
   end
--- db/migrate/20180421204657_remove_category_from_transaction.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCategoryFromTransaction < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :transactions, :category_id, :integer
-  end
-end
